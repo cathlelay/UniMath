5c3b1744cca185c603ab2f809b99904810611731
Catherine Lelay <clelay@ias.edu>
2016-03-03 11:34:51 -0500

    new theorems about hr_abs

--- UniMath/Dedekind/Reals_analysis.v
+++ UniMath/Dedekind/Reals_analysis.v
@@ -9,6 +9,54 @@ Require Export UniMath.Dedekind.Complements.
 
 (** ** NonnegativeReals is an absrng *)
 
+Lemma issepp_hr_abs :
+  ∀ x : hr_commrng, hr_ap_rel x 0%rng <-> (0 < hr_abs x)%NR.
+Proof.
+  intros x.
+  split ; intros H.
+  - apply hr_ap_lt in H.
+    destruct H as [H | H].
+    + apply_pr2_in hr_to_NR_negative H.
+      unfold Rabs, hr_abs.
+      rewrite (pr1 H), maxNonnegativeReals_carac_r.
+      apply ispositive_apNonnegativeReals, (pr2 H).
+      now apply isnonnegative_NonnegativeReals.
+    + apply_pr2_in hr_to_NR_positive H.
+      unfold Rabs, hr_abs.
+      rewrite (pr2 H), maxNonnegativeReals_carac_l.
+      apply ispositive_apNonnegativeReals, (pr1 H).
+      now apply isnonnegative_NonnegativeReals.
+  - apply maxNonnegativeReals_lt' in H.
+    revert H.
+    apply hinhuniv.
+    intros [H | H].
+    + rewrite <- (hr_to_NR_bij x), <- (hr_to_NR_bij 0%rng), hr_to_NR_zero.
+      apply NR_to_hr_ap.
+      change ((hr_to_NRpos x) + 0 ≠ 0 + (hr_to_NRneg x))%NR.
+      rewrite hr_to_NRposneg_zero.
+      rewrite !isrunit_zero_plusNonnegativeReals.
+      now apply_pr2 ispositive_apNonnegativeReals.
+      exact H.
+    + rewrite <- (hr_to_NR_bij x), <- (hr_to_NR_bij 0%rng), hr_to_NR_zero.
+      apply NR_to_hr_ap.
+      change ((hr_to_NRpos x) + 0 ≠ 0 + (hr_to_NRneg x))%NR.
+      rewrite hr_to_NRnegpos_zero.
+      rewrite !islunit_zero_plusNonnegativeReals.
+      apply issymm_apNonnegativeReals.
+      now apply_pr2 ispositive_apNonnegativeReals.
+      exact H.
+Qed.
+
+Lemma hr_abs_one :
+  hr_abs (1%rng : hr_commrng) = 1%NR.
+Proof.
+  unfold hr_abs, hr_to_NRpos, hr_to_NRneg.
+  rewrite hr_to_NR_one.
+  rewrite maxNonnegativeReals_carac_l.
+  reflexivity.
+  apply isnonnegative_NonnegativeReals.
+Qed.
+
 Unset Printing Notations.
 
 Definition R_absrng : absrng NR_NonnegativeRig.
@@ -32,71 +80,15 @@ Proof.
     exact (pr2 (Rplus_apcompat_r c a b)).
   - intros a b c.
     exact (pr2 (Rplus_apcompat_l c a b)).
-  - intros H.
-    change (0 < Rabs x)%NR.
-    simpl in H.
-    destruct (pr1 (hr_NR_ap_0 x (pr1 (hr_to_NR x)) (pr1 (pr2 (hr_to_NR x)))) H) as [H0 | H0] ;
-    apply ispositive_apNonnegativeReals in H0 ;
-    eapply istrans_lt_le_ltNonnegativeReals.
-    apply H0.
-    eapply istrans_leNonnegativeReals, maxNonnegativeReals_le_r.
-    apply minusNonnegativeReals_le.
-    apply H0.
-    eapply istrans_leNonnegativeReals, Dcuts_max_le_l.
-    apply Dcuts_minus_le.
-  - apply hinhuniv.
-    intros (r,(_)).
-    apply hinhuniv.
-    intros H.
-    simple refine (pr2 (hr_NR_ap_0 _ _ _) _).
-    apply (pr1 (hr_to_NR x)).
-    apply (pr1 (pr2 (hr_to_NR x))).
-    destruct (hr_to_NR x) as (X,(Hx,Hle)) ; simpl pr1 in H |- *.
-    destruct H as [Hr | Hr].
-    + right.
-      apply_pr2 ispositive_apNonnegativeReals.
-      eapply istrans_lt_le_ltNonnegativeReals.
-      apply hinhpr.
-      exists r.
-      split.
-      apply Dcuts_zero_empty.
-      apply Hr.
-      simple refine (pr1 (Hle (((pr1 X) - (pr2 X))%NR ,, ((pr2 X) - (pr1 X))%NR) _)).
-      revert Hx.
-      apply hr_inside_carac'.
-      simpl pr1 ; simpl pr2.
-      rewrite iscomm_plusNonnegativeReals, !Dcuts_minus_plus_max.
-      apply iscomm_Dcuts_max.
-    + left.
-      apply_pr2 ispositive_apNonnegativeReals.
-      eapply istrans_lt_le_ltNonnegativeReals.
-      apply hinhpr.
-      exists r.
-      split.
-      apply Dcuts_zero_empty.
-      apply Hr.
-      simple refine (pr2 (Hle (((pr1 X) - (pr2 X))%NR ,, ((pr2 X) - (pr1 X))%NR) _)).
-      revert Hx.
-      apply hr_inside_carac'.
-      simpl pr1 ; simpl pr2.
-      rewrite iscomm_plusNonnegativeReals, !Dcuts_minus_plus_max.
-      apply iscomm_Dcuts_max.
+  - now apply issepp_hr_abs.
+  - now apply_pr2 issepp_hr_abs.
   - rewrite hr_abs_opp.
-    change (@rngunel2 Reals) with Rone.
-    change (hr_abs Rone) with (maxNonnegativeReals (pr1 (RtoNRNR Rone)) (pr2 (RtoNRNR Rone))).
-    rewrite <- NRNRtoR_one.
-    rewrite RtoNRNR_NRNRtoR ; simpl pr1 ; simpl pr2.
-    rewrite <- (minusNonnegativeReals_correct_l _ _ 1%NR).
-    rewrite minusNonnegativeReals_eq_zero.
-    rewrite maxNonnegativeReals_carac_l.
-    reflexivity.
-    apply isnonnegative_NonnegativeReals.
-    apply isnonnegative_NonnegativeReals.
-    apply pathsinv0, islunit_zero_plusNonnegativeReals.
+    exact hr_abs_one.
   - intros x y.
     apply istriangle_Rabs.
   - intros x y.
-    apply Rabs_submult.
+    rewrite Rabs_Rmult.
+    apply isrefl_leNonnegativeReals.
 Defined.
 
 Definition R_NormedModule : NormedModule R_absrng
